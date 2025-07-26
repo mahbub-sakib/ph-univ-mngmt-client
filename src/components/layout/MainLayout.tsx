00c24e0053740942b2fa6c14cbf5d4c8df91eae5
mahbub-sakib <mahbub.sakib418@gmail.com>
2025-07-27 05:29:46 +0600

    till m-5-4 done

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,72 @@
+import { Layout, Menu, MenuProps } from 'antd';
+// import { Content, Footer, Header } from 'antd/es/layout/layout';
+// import Sider from 'antd/es/layout/Sider';
+const { Header, Content, Footer, Sider } = Layout;
+import React from 'react';
+
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+const items: MenuProps['items'] = [
+    {
+        key: '1',
+        label: 'Dashboard',
+    },
+    {
+        key: '2',
+        label: 'Profile',
+    },
+    {
+        key: '3',
+        label: 'User Management',
+        children: [
+            {
+                key: '11',
+                label: 'Create Admin',
+            },
+            {
+                key: '21',
+                label: 'Create Student',
+            },
+        ]
+    }
+]
+
+const MainLayout = () => {
+    return (
+        <Layout style={{ height: '100vh' }}>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div style={{ color: "white", textAlign: 'center', height: '4rem', display: "flex", justifyContent: 'center', alignItems: 'center' }} >
+                    <h1>Univ Management</h1>
+                </div>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, }} />
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+
+                        }}
+                    >
+                        This is my main content
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default MainLayout;
